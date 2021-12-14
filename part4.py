28280ff52c0e00210874b398a801f571ee9e0092
Jack Boge <94186657+SimplyJax123@users.noreply.github.com>
2021-12-14 18:12:36 +0000

    finished code

--- part4.py
+++ part4.py
@@ -40,3 +40,18 @@ fizz
 """
 
 # write your code here 
+num = int(input("enter a number: "))
+
+for i in range(1, num + 1):
+  if i % 3 == 0 and i % 5 == 0:
+    print("fizzbuzz")
+  elif i % 3 == 0:
+    print("fizz")
+  elif i % 5 == 0:
+    print("buzz")
+  else:
+    print(i)
+
+
+
+
